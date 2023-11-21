7062e03f77425ccc2d72c4eec27892df0193f93b
seonhak1~ <tjsgkr134679@gmail.com>
2023-11-21 13:09:11 +0900

    init commit

---
+++ src/test/java/com/best11/gamelog/GameLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.best11.gamelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
